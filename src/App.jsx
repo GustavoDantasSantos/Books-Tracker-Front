988f77c136db603e1e51b3e01fd334437f89867a
Gustavo Vinicius <guvinidantas@gmail.com>
2022-11-18 08:02:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@carbon/react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Olá</h1>
+      <Button>Oie</Button>
+    </div>
+  )
+}
+
+export default App
